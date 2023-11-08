fbd0e39545f7644c744730adee0a9d7508139f9f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-11-07 17:46:52 -0800

    stop hydrating immediately to try islands architecture

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,9 @@ cfg_if! { if #[cfg(feature = "hydrate")] {
         _ = console_log::init_with_level(log::Level::Debug);
         console_error_panic_hook::set_once();
 
-        leptos::mount_to_body(App);
+        // commented out to try islands architecture:
+        // leptos::mount_to_body(App);
+        // added in to replace above in islands architecture:
+        leptos::leptos_dom::HydrationCtx::stop_hydrating();
     }
 }}
