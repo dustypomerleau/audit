095758876cc1830495ed85fd0bc9e3d194c4932a
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-12-05 22:16:36 +1100

    docs on Vision

--- src/case.rs
+++ src/case.rs
@@ -27,6 +27,14 @@ pub enum Formula {
     Kane,
 }
 
+/// A Snellen-style fractional visual acuity, with numerator and denominator. Units are not
+/// specified, but both fields must be in the same unit. This struct is distance-agnostic, and may
+/// represent a true distance acuity, or the distance equivalent of a near acuity. For this reason,
+/// a [`Vision`] must always be wrapped in a [`VaDistance`] or a [`VaNear`], to distinguish the two
+/// cases.
+///
+/// The type of vision chart is left to the surgeon's discretion, but is presumed to be a Snellen,
+/// ETDRS, or similar chart that provides fractional equivalents.
 pub struct Vision {
     num: f32,
     den: f32,
