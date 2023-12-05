c591f0221e1f8caac709eaa604995c9d15f5bdf2
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-12-05 12:39:19 +1100

    make surgeon fields public

--- src/surgeon.rs
+++ src/surgeon.rs
@@ -1,7 +1,7 @@
 /// A unique surgeon
 pub struct Surgeon {
-    email: String, // probably best to validate this as unique and email form at both the form and database levels - but pulling in the regex crate will probably make your wasm bundle huge
-    first_name: String,
-    last_name: String,
-    site: Option<String>,
+    pub email: String, // probably best to validate this as unique and email form at both the form and database levels - but pulling in the regex crate will probably make your wasm bundle huge
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub site: Option<String>,
 }
