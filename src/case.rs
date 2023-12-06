13cda1ba145e285ea1f5060b87ceecb64911fef4
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-12-06 16:51:43 +1100

    add constants/slices for dioptric power ranges

--- src/case.rs
+++ src/case.rs
@@ -3,6 +3,34 @@ use crate::surgeon::Surgeon;
 use serde::Deserialize;
 use time::{Date, OffsetDateTime};
 
+/// A generic representation of all possible powers (diopters), such as those used in IOLs and refractions (-20.0 D to +60.0 D in 0.25 D steps).
+const POWERS: [f32; 321] = make_powers(); // -20.0 to +60.0
+
+/// A range of powers (diopters) for the spherical component of a subjective refraction (-20.0 D to
+/// +20.0 D in 0.25 D steps).
+const REF_SPH_POWERS: &[f32] = &POWERS[0..161]; // -20.0 to +20.0
+
+/// A range of powers (diopters) for the cylinder component of a subjective refraction (-10.0 D to
+/// +10.0 D in 0.25 D steps)
+const REF_CYL_POWERS: &[f32] = &POWERS[40..121]; // -10.0 to +10.0
+
+/// A range of powers (diopters) for the spherical equivalent labeling of an IOL (-20.0 D to +60.0
+/// D in 0.25 D steps).
+const IOL_SE_POWERS: &[f32] = &POWERS[..]; // -20.0 to +60.0
+
+/// A range of powers (diopters) for the cylinder power of an IOL (+1.0 to +20.0
+/// D in 0.25 D steps, IOL plane).
+const IOL_CYL_POWERS: &[f32] = &POWERS[84..161]; // +1.0 to +20.0
+
+fn make_powers() -> [f32; 321] {
+    let mut powers = [0.0; 321];
+    powers[0] = -20.0;
+    for i in 1..321 {
+        powers[i] = powers[i - 1] + 0.25;
+    }
+    powers
+}
+
 // leave off Both, until you have a specific use case for it
 pub enum Side {
     Right,
