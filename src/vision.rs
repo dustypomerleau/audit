d38934118b1a70efcd140fb493f24d449bd25f17
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-12-29 19:01:53 +0800

    move to an enum-based Va system

--- src/vision.rs
+++ src/vision.rs
@@ -1,65 +1,60 @@
 /// A Snellen-style fractional visual acuity, with numerator and denominator. Units are not
-/// specified, but both fields must be in the same unit. This struct is distance-agnostic, and may
-/// represent a true distance acuity, or the distance equivalent of a near acuity. For this reason,
-/// a [`Vision`] must always be wrapped in a [`VaDistance`] or a [`VaNear`], to distinguish the two
-/// cases.
+/// specified, but both fields must be in the same unit.  
 ///
 /// The type of vision chart is left to the surgeon's discretion, but is presumed to be a Snellen,
 /// ETDRS, or similar chart that provides fractional equivalents.
-// todo: another option would be to get rid of VaDistance and VaNear and make this an enum, which
-// on reflection seems better, but it may not allow enforcing which enum variant can occupy a given
-// struct field
-// something like:
-// pub enum Vision {
-//     OutOfBounds,
-//     Distance { num: f32, den: f32 },
-//     Near { num: f32, den: f32 },
-// }
-// downside would be that you would need to match on the type whenever you access a field holding a
-// Vision...
 #[derive(Debug, PartialEq)]
-pub struct Vision {
-    num: f32,
-    den: f32,
+pub enum Va {
+    OutOfBounds(BadVa),
+    Distance { num: f32, den: f32 },
+    Near { num: f32, den: f32 },
 }
 
-impl Vision {
-    pub fn new(num: f32, den: f32) -> Option<Self> {
-        if (0.1..=20.0).contains(&num) && den > 0.0 {
-            Some(Self { num, den })
+pub enum VaKind {
+    Distance,
+    Near,
+}
+
+pub enum BadVa {
+    Num,
+    Den,
+}
+
+impl Va {
+    pub fn new(kind: VaKind, num: f32, den: f32) -> Self {
+        if (0.1..=20.0).contains(&num) {
+            if den > 0.0 {
+                match kind {
+                    VaKind::Distance => Self::Distance { num, den },
+                    VaKind::Near => Self::Near { num, den },
+                }
+            } else {
+                Self::OutOfBounds(BadVa::Den)
+            }
         } else {
-            None
+            Self::OutOfBounds(BadVa::Num)
         }
     }
 }
 
-/// A Snellen-style fractional visual acuity measured at distance.
-#[derive(Debug, PartialEq)]
-pub struct VaDistance(pub Vision);
-
-/// A Snellen-style fractional visual acuity measured at near, and converted to its distance
-/// equivalent.
-#[derive(Debug, PartialEq)]
-pub struct VaNear(pub Vision);
+struct VaSet {
+    before: Va,
+    after: Va,
+}
 
 /// A collection of preoperative and postoperative visual acuity measurements for a given case. The
 /// best-corrected preoperative visual acuity and the best-corrected postoperative visual acuity
 /// are mandatory. Near and uncorrected (raw) visual acuities are optional.
-// todo: consider nesting instead of these field names
-// something like OpVision { before: { distance: { best: VaDistance, raw: Option<VaDistance> }}}
+// todo: perhaps we want a new() function that ensures the correct enum variants for each VaSet
+// in OpVision
+// Note: this structure forces you to have both a before and an after for a given type of
+// acuity (meaning that only full sets are allowed). I think this is desireable, because
+// analysis is only possible if you have before and after datea, but if it
+// causes headaches, you may need to rethink it.
 #[derive(Debug, PartialEq)]
-pub struct OpVision {
-    best_before: VaDistance,
-    raw_before: Option<VaDistance>,
-
-    best_after: VaDistance,
-    raw_after: Option<VaDistance>,
-
-    best_near_before: Option<VaNear>,
-    raw_near_before: Option<VaNear>,
-
-    best_near_after: Option<VaNear>,
-    raw_near_after: Option<VaNear>,
+pub struct OpVa {
+    best_distance: VaSet,
+    best_near: Option<VaSet>,
+    raw_distance: Option<VaSet>,
+    raw_near: Option<VaSet>,
 }
-
-pub trait Va {} // todo: for common methods on acuity
