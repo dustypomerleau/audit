96707a3651f3c8b725acc6881a26971f5e6109db
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-12-30 15:00:28 +0800

    make `TargetCyl` an enum with `OutOfBounds` variant, for more accurate errors

--- src/target.rs
+++ src/target.rs
@@ -1,4 +1,4 @@
-use crate::case::Axis;
+use crate::{axis::Axis, sca::BadSca};
 
 /// A formula for calculating IOL power from biometry.
 // Limited to common thick-lens formulas to start.
@@ -9,11 +9,14 @@ pub enum Formula {
     Kane,
 }
 
+/// A newtype to hold powers compatible with a target cylinder value.
 #[derive(Debug, PartialEq)]
 pub struct TargetCylPower(f32);
 
 impl TargetCylPower {
-    pub fn new(value: f32) -> Option<Self> {
+    /// Creates a new [`TargetCylPower`] of up to 6 diopters, returning `None` if the value is out
+    /// of bounds.
+    pub fn new(power: f32) -> Option<Self> {
         if (0.0..=6.0).contains(&value) {
             Some(Self(value))
         } else {
@@ -22,24 +25,26 @@ impl TargetCylPower {
     }
 }
 
+/// A [`Target`] cylinder value, including power and axis.
 #[derive(Debug, PartialEq)]
-pub struct TargetCyl {
-    power: TargetCylPower,
-    axis: Axis,
+pub enum TargetCyl {
+    OutOfBounds(BadSca),
+    Value { power: TargetCylPower, axis: Axis },
 }
 
 impl TargetCyl {
-    fn new(power: Option<f32>, axis: Option<i32>) -> Option<Self> {
-        match (power, axis) {
-            (Some(power), Some(axis)) => {
-                if let (Some(power), Some(axis)) = (TargetCylPower::new(power), Axis::new(axis)) {
-                    Some(Self { power, axis })
-                } else {
-                    None
-                }
+    /// Returns a new [`TargetCyl::Value`] if both the [`TargetCylPower`] and the [`Axis`] are within
+    /// bounds. Returns [`TargetCyl::OutOfBounds`] with the offending field if either is out of
+    /// bounds.
+    fn new(power: f32, axis: f32) -> Self {
+        if let Some(power) = TargetCylPower::new(power) {
+            if let Some(axis) = Axis::new(axis) {
+                Self::Value { power, axis }
+            } else {
+                Self::OutOfBounds(BadSca::Axis)
             }
-
-            (_, _) => None,
+        } else {
+            Self::OutOfBounds(BadSca::Cyl)
         }
     }
 }
@@ -47,25 +52,43 @@ impl TargetCyl {
 /// The residual postop refraction predicted by your formula of choice.
 // At the start, allow only one formula/target.
 #[derive(Debug, PartialEq)]
-pub struct Target {
-    formula: Option<Formula>,
-    se: f32,
-    cyl: Option<TargetCyl>, // todo: confirm which plane the biometry is predicting, IOL or corneal
+pub enum Target {
+    OutOfBounds(BadSca),
+    Value {
+        formula: Option<Formula>,
+        se: f32,
+        cyl: Option<TargetCyl>,
+    },
 }
 
 impl Target {
-    pub fn new(
-        formula: Option<Formula>,
-        se: f32,
-        cyl: Option<f32>,
-        axis: Option<i32>,
-    ) -> Option<Self> {
-        let cyl = TargetCyl::new(cyl, axis);
-
+    /// Returns a new [`Target`], giving the [`Target::Value`] variant if the spherical equivalent is within bounds,
+    /// and [`Target::OutOfBounds`] if the `se` is out of bounds. The `cyl` field only contains
+    /// `Some(TargetCyl)` if both `cyl` and `axis` are within bounds.
+    pub fn new(formula: Option<Formula>, se: f32, cyl: Option<f32>, axis: Option<i32>) -> Self {
         if (-6.0..=2.0).contains(&se) {
-            Some(Self { formula, se, cyl })
+            match (cyl, axis) {
+                (Some(cyl), Some(axis)) => {
+                    let cyl = TargetCyl::new(cyl, axis);
+                    let cyl = match cyl {
+                        TargetCyl::Value => Some(cyl),
+                        // todo: log bad cyl, throw an error explaining that cyl is out of bounds
+                        TargetCyl::OutOfBounds(BadSca::Cyl) => None,
+                        // todo: log bad axis, throw an error explaining that axis is out of bounds
+                        TargetCyl::OutOfBounds(BadSca::Axis) => None,
+                    };
+
+                    Self::Value { formula, se, cyl }
+                }
+
+                _ => Self::Value {
+                    formula,
+                    se,
+                    cyl: None,
+                },
+            }
         } else {
-            None
+            Self::OutOfBounds(BadSca::Sph)
         }
     }
 }
