abfab303e73545151832662904a0f357150ecc50
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-11 21:21:38 +1100

    documentation for `Distance`

--- src/distance.rs
+++ src/distance.rs
@@ -1,5 +1,7 @@
+/// A distance wrapper to ensure that distance and near values ([`Refraction`](crate::refraction::Refraction), [`Va`](crate::va::Va)) are not confused.
 #[derive(Debug, PartialEq)]
 pub struct Distance<T>(T);
 
+/// A near wrapper to ensure that distance and near values ([`Refraction`](crate::refraction::Refraction), [`Va`](crate::va::Va)) are not confused.
 #[derive(Debug, PartialEq)]
 pub struct Near<T>(T);
