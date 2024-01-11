bcc61fe72cf3e13c98c51ec8ce0300c28b8922f8
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-11 21:15:43 +1100

    documentation for `Cyl`

--- src/cyl.rs
+++ src/cyl.rs
@@ -1,6 +1,6 @@
 use crate::axis::Axis;
 
-/// A parent-agnostic cylinder type. The acceptable bounds of [`power`](Cyl::power) depend on the
+/// An agnostic cylinder type. The acceptable bounds of [`power`](Cyl::power) depend on the
 /// type of power being represented. Since the acceptable values of [`axis`](Cyl::axis) are always
 /// the same, we insist upon an [`Axis`](crate::axis::Axis) to constrain it.
 #[derive(Debug, PartialEq)]
@@ -9,6 +9,7 @@ pub struct Cyl {
     pub axis: Axis,
 }
 
+/// Representation of missing cylinder components for use by error types.
 #[derive(Debug, PartialEq)]
 pub enum CylPair {
     Power,
