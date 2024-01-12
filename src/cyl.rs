fabec1d90c77b68bcd5b65ffe2748a8b1839924f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-12 22:30:55 +1100

    add `Cyl::new()`

--- src/cyl.rs
+++ src/cyl.rs
@@ -1,4 +1,4 @@
-use crate::axis::Axis;
+use crate::{axis::Axis, sca::ScaBoundsError};
 
 /// An agnostic cylinder type. The acceptable bounds of [`power`](Cyl::power) depend on the
 /// type of power being represented. Since the acceptable values of [`axis`](Cyl::axis) are always
@@ -7,6 +7,27 @@ use crate::axis::Axis;
 pub struct Cyl {
     pub power: f32,
     pub axis: Axis,
+}
+
+// it makes sense to have the more primitive constructors take option, so we can construct
+// them directly from the DB. However, if you're implementing this logic here, you don't need
+// to repeat it, just call Cyl::new in Sca::new.
+impl Cyl {
+    pub fn new(power: Option<f32>, axis: Option<i32>) -> Result<Self, ScaBoundsError> {
+        match (power, axis) {
+            (Some(power), Some(axis)) => {
+                if let Some(axis) = Axis::new(axis) {
+                    Ok(Self { power, axis })
+                } else {
+                    Err(ScaBoundsError::Axis(axis))
+                }
+            }
+
+            (Some(power), _) => Err(ScaBoundsError::NoPair(CylPair::Axis)),
+
+            (_, _) => Err(ScaBoundsError::NoPair(CylPair::Power)),
+        }
+    }
 }
 
 /// Representation of missing cylinder components for use by error types.
