2b82d3a5ef8ad5447dcbf77f1e98c30f27bc2298
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-13 19:19:15 +1100

    reduce `Iol::cyl` bounds check to a single expression

--- src/iol.rs
+++ src/iol.rs
@@ -33,17 +33,11 @@ impl TryFrom<Sca> for Iol {
         let Sca { sph, cyl } = sca;
 
         if (-20.0..=60.0).contains(&sph) && sph % 0.25 == 0.0 {
-            match cyl {
-                Some(Cyl { power, axis: _ }) => {
-                    if (1.0..=20.0).contains(&power) && power % 0.25 == 0.0 {
-                        Ok(Self(sca))
-                    } else {
-                        Err(IolBoundsError::Cyl(power))
-                    }
-                }
-
-                None => Ok(Self(sca)),
+            if cyl.is_some() && (!(1.0..=20.0).contains(&cyl.power) || !cyl.power % 0.25 == 0.0) {
+                return Err(IolBoundsError::Cyl(power));
             }
+
+            Ok(Self(sca))
         } else {
             Err(IolBoundsError::Se(sph))
         }
