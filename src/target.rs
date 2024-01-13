e535203bb21a7fd3eb0ada26fa8c798dcae76d20
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-13 18:45:05 +1100

    use `Sca` to build `Target`

--- src/target.rs
+++ src/target.rs
@@ -1,16 +1,14 @@
 use crate::{
     axis::Axis,
     cyl::{Cyl, CylPair},
+    sca::{Sca, ScaBoundsError},
 };
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum TargetBoundsError {
-    #[error("target must always have a spherical equivalent, but `None` was supplied")]
-    NoSph,
-
-    #[error("target cylinder must have both a power and an axis but the {0:?} was not supplied")]
-    NoPair(CylPair),
+    #[error("target cannot be created because the underlying Sca violated its invariants: {0:?}")]
+    Sca(ScaBoundsError),
 
     #[error(
         "target spherical equivalent must be a value between -6 D and +2 D (supplied value: {0})"
@@ -19,9 +17,6 @@ pub enum TargetBoundsError {
 
     #[error("target cylinder power must be a value between 0 D and +6 D (supplied value: {0})")]
     Cyl(f32),
-
-    #[error("target axis must be an integer value between 0° and 179° (supplied value: {0})")]
-    Axis(i32),
 }
 
 /// A formula for calculating IOL power from biometry.
@@ -38,47 +33,21 @@ pub enum Formula {
 #[derive(Debug, PartialEq)]
 pub struct Target {
     formula: Option<Formula>,
-    se: f32,
-    cyl: Option<Cyl>,
+    sca: Sca,
 }
 
 impl Target {
-    pub fn new(
-        formula: Option<Formula>,
-        se: f32,
-        cyl: Option<f32>,
-        axis: Option<i32>,
-    ) -> Result<Self, TargetBoundsError> {
-        if (-6.0..=2.0).contains(&se) {
-            match (cyl, axis) {
-                (Some(cyl), Some(axis)) => {
-                    if (0.0..=6.0).contains(&cyl) {
-                        if let Some(axis) = Axis::new(axis) {
-                            Ok(Self {
-                                formula,
-                                se,
-                                cyl: Some(Cyl { power: cyl, axis }),
-                            })
-                        } else {
-                            Err(TargetBoundsError::Axis(axis))
-                        }
-                    } else {
-                        Err(TargetBoundsError::Cyl(cyl))
-                    }
-                }
+    fn new(formula: Option<Formula>, sca: Sca) -> Result<Self, Self::Error> {
+        let Sca { sph, cyl } = sca;
 
-                (Some(_cyl), _) => Err(TargetBoundsError::NoPair(CylPair::Axis)),
-
-                (_, Some(_axis)) => Err(TargetBoundsError::NoPair(CylPair::Power)),
-
-                (_, _) => Ok(Self {
-                    formula,
-                    se,
-                    cyl: None,
-                }),
+        if (-6.0..=2.0).contains(&sph) {
+            if cyl.is_some() && !(0.0..=6.0).contains(&cyl.power) {
+                Err(TargetBoundsError::Cyl(power))
             }
+
+            Ok(Self { formula, sca })
         } else {
-            Err(TargetBoundsError::Se(se))
+            Err(TargetBoundsError::Se(sph))
         }
     }
 }
