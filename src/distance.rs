b3cdc294972c52fae63bd1580eeb6064c998bb7d
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-17 13:22:35 +1100

    add `From<Va>` and `From<Refraction>` for `Distance`

--- src/distance.rs
+++ src/distance.rs
@@ -1,7 +1,27 @@
-/// A distance wrapper to ensure that distance and near values ([`Refraction`](crate::refraction::Refraction), [`Va`](crate::va::Va)) are not confused.
+use crate::refraction::Refraction;
+
+/// A distance wrapper to ensure that distance and near values
+/// ([`Refraction`](crate::refraction::Refraction), [`Va`](crate::va::Va)) are not confused.
 #[derive(Debug, PartialEq)]
 pub struct Distance<T>(T);
 
-/// A near wrapper to ensure that distance and near values ([`Refraction`](crate::refraction::Refraction), [`Va`](crate::va::Va)) are not confused.
+impl From<Va> for Distance<Va> {
+    fn from(va: Va) -> Self { Self(va) }
+}
+
+impl From<Refraction> for Distance<Refraction> {
+    fn from(refraction: Refraction) -> Self { Self(refraction) }
+}
+
+/// A near wrapper to ensure that distance and near values
+/// ([`Refraction`](crate::refraction::Refraction), [`Va`](crate::va::Va)) are not confused.
 #[derive(Debug, PartialEq)]
 pub struct Near<T>(T);
+
+impl From<Va> for Near<Va> {
+    fn from(va: Va) -> Self { Self(va) }
+}
+
+impl From<Refraction> for Near<Refraction> {
+    fn from(refraction: Refraction) -> Self { Self(refraction) }
+}
