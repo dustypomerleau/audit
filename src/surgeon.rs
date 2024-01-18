d4f1eef8319f22ed6974e4fa071303d1e80fed30
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-19 09:42:57 +1100

    notes

--- src/surgeon.rs
+++ src/surgeon.rs
@@ -1,7 +1,13 @@
 /// A unique surgeon
+// In the DB, the Surgeon type will have an SIA for right and left eyes, but we don't need that
+// value here. After hitting the DB, either the value for `FlatCase::sia` will be `None` (in which
+// case we use the surgeon's default value for that side), or it will be `Some()`, in which case
+// that case-specific value will override the surgeon's defaults.
 #[derive(Debug, PartialEq)]
 pub struct Surgeon {
-    pub email: String, // probably best to validate this as unique and email form at both the form and database levels - but pulling in the regex crate will probably make your wasm bundle huge
+    // probably best to validate this as unique and email form at both the form and database levels
+    // - but pulling in the regex crate will probably make your wasm bundle huge
+    pub email: String,
     pub first_name: Option<String>,
     pub last_name: Option<String>,
     pub site: Option<String>,
