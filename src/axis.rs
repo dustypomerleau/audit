ce3752a9bc46e48cd195647838b5712a91c4d408
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-18 11:03:08 +1100

    tests for `Axis::new`

--- src/axis.rs
+++ src/axis.rs
@@ -13,3 +13,25 @@ impl Axis {
         }
     }
 }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn makes_new_axis() {
+        let axis = Axis::new(90);
+        assert_eq!(axis, Some(Axis(90i32)))
+    }
+
+    #[test]
+    fn negative_axis_returns_none() {
+        let axis = Axis::new(-1);
+        assert_eq!(axis, None)
+    }
+
+    #[test]
+    fn out_of_bounds_axis_returns_none() {
+        let axis = Axis::new(300);
+        assert_eq!(axis, None)
+    }
+}
