d777e3084ab8cd9f5fb4e72e5e5a8fb50735125a
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-30 16:37:23 +1100

    refactor(Va): complete rework of `OpVa` fields:
    - split `Va`s into before and after sets with different required fields
    - return to wrapper types for `Near` and `Far`

--- src/va.rs
+++ src/va.rs
@@ -1,7 +1,7 @@
-use crate::distance::{Distance, Far, Near};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
+/// A representation of a missing member of a fractional visual acuity, for use by error types.
 #[derive(Debug, PartialEq)]
 pub enum VaPair {
     Numerator,
@@ -18,6 +18,14 @@ pub enum VaBoundsError {
     NoPair(VaPair),
 }
 
+/// A wrapper type, to ensure that far visual acuities are only compared to other far acuities.
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Far(Va);
+
+/// A wrapper type, to ensure that near visual acuities are only compared to other near acuities.
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Near(Va);
+
 /// A Snellen-style fractional visual acuity, with numerator and denominator. Units are not
 /// specified, but both fields must be in the same unit.  
 ///
@@ -30,6 +38,7 @@ pub struct Va {
 }
 
 impl Va {
+    /// Creates a new visual acuity with bounds checking.
     pub fn new(num: f32, den: f32) -> Result<Self, VaBoundsError> {
         if (0.0..=20.0).contains(&num) && num > 0.0 {
             if den > 0.0 {
@@ -43,24 +52,31 @@ impl Va {
     }
 }
 
+/// A collection of visual acuities from before surgery. We use separate structs for [`BeforeVa`]
+/// and [`AfterVa`], because we enforce different mandatory fields for the two situations.
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-pub struct VaSet<T>
-where T: Distance<Va>
-{
-    pub before: T,
-    pub after: T,
+pub struct BeforeVa {
+    best_far: Far<Va>,
+    best_near: Option<Near<Va>>,
+    raw_far: Option<Far<Va>>,
+    raw_near: Option<Near<Va>>,
 }
 
-/// A collection of preoperative and postoperative visual acuity measurements for a given case.
-/// The `VaSet` for best-corrected far acuity is mandatory. Other fields are optional.
-// todo: perhaps we want a new() function that ensures the correct enum variants for each VaSet
-// in OpVa
+/// A collection of visual acuities from after surgery. We use separate structs for [`BeforeVa`]
+/// and [`AfterVa`], because we enforce different mandatory fields for the two situations.
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct AfterVa {
+    best_far: Option<Far<Va>>,
+    best_near: Option<Near<Va>>,
+    raw_far: Far<Va>,
+    raw_near: Option<Near<Va>>,
+}
+
+/// The visual acuity sets from before and after a particular [`Case`](crate::case::Case).
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct OpVa {
-    pub best_far: VaSet<Far<Va>>,
-    pub best_near: Option<VaSet<Near<Va>>>,
-    pub raw_far: Option<VaSet<Far<Va>>>,
-    pub raw_near: Option<VaSet<Near<Va>>>,
+    before: BeforeVa,
+    after: AfterVa,
 }
 
 mod tests {
@@ -69,20 +85,23 @@ mod tests {
     #[test]
     fn makes_new_va() {
         let va = Va::new(6.0, 7.5).unwrap();
+
         assert_eq!(va, Va { num: 6.0, den: 7.5 })
     }
 
     #[test]
     fn out_of_bounds_va_numerator_returns_err() {
-        let num = 21.2f32;
+        let num = 21.2;
         let va = Va::new(num, 9.0);
+
         assert_eq!(va, Err(VaBoundsError::Num(num)))
     }
 
     #[test]
     fn out_of_bounds_va_denominator_returns_err() {
-        let den = -1.2f32;
+        let den = -1.2;
         let va = Va::new(6.0, den);
+
         assert_eq!(va, Err(VaBoundsError::Den(den)))
     }
 }
