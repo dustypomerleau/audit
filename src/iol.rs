85c6615a89ec84a653e19bd0eb5fc50268a7f25c
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-01-30 14:49:56 +1100

    refactor(Iol): separate the IOL model from the `OpIol` with `Sca`

--- src/iol.rs
+++ src/iol.rs
@@ -1,6 +1,7 @@
 use crate::{
     cyl::{Cyl, CylPair},
     sca::Sca,
+    target::Formula,
 };
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
@@ -25,28 +26,43 @@ pub enum IolBoundsError {
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-// todo: see your DB schema - you need Iol as a full struct with several components, and
-// Iol::new() instead of TryFrom<Sca>, and then OpIol wrapping for Case
-pub struct Iol(pub Sca);
+pub enum Focus {
+    Mono,
+    Edof,
+    Multi,
+}
 
-impl TryFrom<Sca> for Iol {
-    type Error = IolBoundsError;
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Iol {
+    model: String,
+    name: String,
+    focus: Focus,
+    toric: bool,
+}
 
-    fn try_from(sca: Sca) -> Result<Self, Self::Error> {
+pub struct OpIol {
+    iol: Iol,
+    sca: Sca,
+}
+
+impl OpIol {
+    fn new(iol: Iol, sca: Sca) -> Result<Self, IolBoundsError> {
         let Sca { sph, cyl } = sca;
 
         if (-20.0..=60.0).contains(&sph) && sph % 0.25 == 0.0 {
-            match cyl {
-                Some(Cyl { power, axis: _ }) => {
+            let sca = match cyl {
+                Some(Cyl { power, .. }) => {
                     if (1.0..=20.0).contains(&power) && power % 0.25 == 0.0 {
-                        Ok(Self(sca))
+                        sca
                     } else {
-                        Err(IolBoundsError::Cyl(power))
+                        return Err(IolBoundsError::Cyl(power));
                     }
                 }
 
-                None => Ok(Self(sca)),
-            }
+                None => sca,
+            };
+
+            Ok(Self { iol, sca })
         } else {
             Err(IolBoundsError::Se(sph))
         }
@@ -55,60 +71,64 @@ impl TryFrom<Sca> for Iol {
 
 mod tests {
     use super::*;
-    use crate::axis::Axis;
+
+    // todo: replace this function with an implementation of Mock
+    fn iol() -> Iol {
+        Iol {
+            model: "ZXTxxx".to_string(),
+            name: "Tecnis Symfony".to_string(),
+            focus: Focus::Edof,
+            toric: true,
+        }
+    }
 
     #[test]
-    fn iol_implements_try_from_sca() {
-        let iol: Iol = Sca::new(24.25, Some(3.0), Some(12))
-            .unwrap()
-            .try_into()
-            .unwrap();
+    fn makes_new_opiol() {
+        let iol = iol();
+        let sca = Sca::new(24.25, Some(3.0), Some(12))?;
+        let opiol = OpIol::new(iol, sca).unwrap();
 
-        assert_eq!(
-            iol,
-            Iol(Sca {
-                sph: 24.25,
-                cyl: Some(Cyl {
-                    power: 3.0,
-                    axis: Axis(12)
-                })
-            })
-        )
+        assert_eq!(opiol, OpIol { iol, sca })
     }
 
     #[test]
     fn out_of_bounds_iol_se_returns_err() {
-        let se = 100.25f32;
-        let iol: Result<Iol, IolBoundsError> =
-            Sca::new(se, Some(3.0), Some(12)).unwrap().try_into();
+        // todo: randomize the out of bounds value
+        let se = 100.25;
+        let iol = iol();
+        let sca = Sca::new(se, Some(3.0), Some(12)).unwrap();
+        let opiol = OpIol::new(iol, sca);
 
-        assert_eq!(iol, Err(IolBoundsError::Se(se)))
+        assert_eq!(opiol, Err(IolBoundsError::Se(se)))
     }
 
     #[test]
     fn nonzero_rem_iol_se_returns_err() {
-        let se = 10.35f32;
-        let iol: Result<Iol, IolBoundsError> =
-            Sca::new(se, Some(3.0), Some(12)).unwrap().try_into();
+        let se = 10.35;
+        let iol = iol();
+        let sca = Sca::new(se, Some(3.0), Some(12)).unwrap();
+        let opiol = OpIol::new(iol, sca);
 
-        assert_eq!(iol, Err(IolBoundsError::Se(se)))
+        assert_eq!(opiol, Err(IolBoundsError::Se(se)))
     }
 
     #[test]
     fn out_of_bounds_iol_cyl_power_returns_err() {
-        let cyl = 31.0f32;
-        let iol: Result<Iol, IolBoundsError> =
-            Sca::new(12.5, Some(cyl), Some(12)).unwrap().try_into();
+        let cyl = 31.0;
+        let iol = iol();
+        let sca = Sca::new(18.5, Some(cyl), Some(170)).unwrap();
+        let opiol = OpIol::new(iol, sca);
 
-        assert_eq!(iol, Err(IolBoundsError::Cyl(cyl)))
+        assert_eq!(opiol, Err(IolBoundsError::Se(se)))
     }
 
     #[test]
     fn nonzero_rem_iol_cyl_power_returns_err() {
-        let cyl = 2.06f32;
-        let iol: Result<Iol, IolBoundsError> =
-            Sca::new(12.5, Some(cyl), Some(12)).unwrap().try_into();
+        let cyl = 2.06;
+        let iol = iol();
+        let sca = Sca::new(28.5, Some(cyl), Some(170)).unwrap();
+        let opiol = OpIol::new(iol, sca);
 
-        assert_eq!(iol, Err(IolBoundsError::Cyl(cyl)))
+        assert_eq!(opiol, Err(IolBoundsError::Cyl(cyl)))
     }
 }
