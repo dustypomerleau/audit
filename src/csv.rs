ed52500abeb5676422d4f30f6df2bf1898a2cc02
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-02-07 15:13:04 +1100

    feat(CSV): test CSV reading

--- src/csv.rs
+++ src/csv.rs
@@ -1,2 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use polars::lazy::frame::{LazyCsvReader, LazyFileListReader};
 
+    #[test]
+    fn reads_csv() {
+        let lf = LazyCsvReader::new("test/test.csv")
+            .finish()
+            .unwrap()
+            .collect();
 
+        assert!(lf.is_ok());
+
+        let df = lf.unwrap();
+        println!("{df}");
+    }
+}
