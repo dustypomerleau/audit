2333168868b12440775199aa8db87b593aff609d
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-02-11 16:28:14 +1100

    feat(csv) use a newtype to satisfy trait bounds for `JsonWriter`

--- src/csv.rs
+++ src/csv.rs
@@ -1,17 +1,46 @@
+use std::io::Write;
+
+#[derive(Clone, Debug)]
+pub struct WriteString(pub String);
+
+impl Write for WriteString {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        buf.iter().for_each(|c| self.0.push(*c as char));
+        Ok(buf.len())
+    }
+
+    // This satisfies rustc, but I'm not actually sure how to implement `flush`.
+    fn flush(&mut self) -> std::io::Result<()> { Ok(()) }
+}
+
 #[cfg(test)]
 mod tests {
-    use polars::lazy::frame::{LazyCsvReader, LazyFileListReader};
+    use crate::{csv::WriteString, flatcase::FlatCase};
+    use polars::{
+        io::{
+            json::{JsonFormat, JsonWriter},
+            SerWriter,
+        },
+        lazy::frame::{LazyCsvReader, LazyFileListReader},
+    };
 
     #[test]
     fn reads_csv() {
-        let lf = LazyCsvReader::new("test/test.csv")
+        let mut df = LazyCsvReader::new("test/test.csv")
             .finish()
             .unwrap()
-            .collect();
+            .collect()
+            .unwrap();
 
-        assert!(lf.is_ok());
-
-        let df = lf.unwrap();
         println!("{df}");
+
+        let mut ws = WriteString(String::new());
+
+        JsonWriter::new(&mut ws)
+            .with_json_format(JsonFormat::Json)
+            .finish(&mut df)
+            .unwrap();
+
+        println!("{}", ws.0);
     }
 }
