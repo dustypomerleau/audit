eabf9db89c5cced8e30fc51543012cfc950a953a
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-07-09 23:30:39 +1000

    fix: replace contents of error_template with new template

--- src/error_template.rs
+++ src/error_template.rs
@@ -1,12 +1,8 @@
-use cfg_if::cfg_if;
 use http::status::StatusCode;
-#[cfg(feature = "ssr")]
-use leptos::use_context;
 use leptos::{
-    component, create_rw_signal, tracing, view, Errors, For, IntoView, RwSignal, SignalGetUntracked,
+    component, create_rw_signal, use_context, view, Errors, For, IntoView, RwSignal,
+    SignalGetUntracked,
 };
-#[cfg(feature = "ssr")]
-use leptos_axum::ResponseOptions;
 use thiserror::Error;
 
 #[derive(Clone, Debug, Error)]
@@ -49,12 +45,14 @@ pub fn ErrorTemplate(
 
     // Only the response code for the first error is actually sent from the server
     // this may be customized by the specific application
-    cfg_if! { if #[cfg(feature="ssr")] {
+    #[cfg(feature = "ssr")]
+    {
+        use leptos_axum::ResponseOptions;
         let response = use_context::<ResponseOptions>();
         if let Some(response) = response {
             response.set_status(errors[0].status_code());
         }
-    }}
+    }
 
     view! {
         <h1>{if errors.len() > 1 {"Errors"} else {"Error"}}</h1>
