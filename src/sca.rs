64cf0492006e396b9fa8a102fae1113393d03f4f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-07-10 19:54:21 +1000

    add back a `RawSca` with `new()` to make constructing `Sca` derivatives easier

--- src/sca.rs
+++ src/sca.rs
@@ -23,3 +23,21 @@ pub trait ScaMut {
     fn set_sph(self, sph: f32) -> Self;
     fn set_cyl(self, cyl: Option<Cyl>) -> Self;
 }
+
+pub struct RawSca {
+    pub sph: f32,
+    pub cyl: Option<Cyl>,
+}
+
+impl RawSca {
+    pub fn new(sph: f32, power: Option<f32>, axis: Option<i32>) -> Result<Self, ScaBoundsError> {
+        let cyl = match (power, axis) {
+            (Some(power), Some(axis)) => Some(Cyl::new(power, axis)?),
+            (None, None) => None,
+            (Some(_cyl), _) => return Err(ScaBoundsError::NoPair(CylPair::Axis)),
+            (_, Some(_axis)) => return Err(ScaBoundsError::NoPair(CylPair::Power)),
+        };
+
+        Ok(Self { sph, cyl })
+    }
+}
