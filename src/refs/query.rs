618ef095bfb759a3eb63f2e08db56b7361b78101
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-09-27 16:10:50 +1000

    test(db): sample Case function

--- src/refs/query.rs
+++ src/refs/query.rs
@@ -3,12 +3,131 @@
 // use crate::case::Case;
 // use edgedb_protocol::{model::Uuid, named_args, value::Value};
 
-#[cfg(feature = "ssr")]
+#[cfg(test)]
 mod tests {
-    use crate::sia::Sia;
+    use crate::{
+        bounds_check::{BoundsCheck, Checked, Unchecked},
+        case::{Adverse, Case, Side},
+        cyl::Cyl,
+        iol::{Focus, Iol, OpIol},
+        refraction::{OpRefraction, Refraction},
+        sia::Sia,
+        surgeon::{Email, Surgeon, SurgeonSia},
+        target::{Constant, Formula, Target},
+        va::{AfterVa, BeforeVa, OpVa, Va},
+    };
+    use chrono::NaiveDate;
     use edgedb_protocol::{named_args, value::Value, QueryResult};
     use edgedb_tokio::create_client;
+    use std::marker::PhantomData;
     use tokio::test;
+
+    fn sample_case() -> Case {
+        Case {
+            surgeon: Surgeon {
+                email: Email::new("email@email.com").unwrap(),
+                first_name: Some("john".to_string()),
+                last_name: Some("smith".to_string()),
+                sites: Some(vec![
+                    "Royal Melbourne Hospital".to_string(),
+                    "Manningham Private Hospital".to_string(),
+                ]),
+                sia: Some(SurgeonSia {
+                    right: Sia {
+                        power: 010,
+                        axis: 100,
+                    },
+                    left: Sia {
+                        power: 010,
+                        axis: 100,
+                    },
+                }),
+            },
+
+            urn: "abc123".to_string(),
+            side: Side::Right,
+
+            target: Some(
+                Target::<Unchecked> {
+                    constant: Some(Constant {
+                        value: 11936,
+                        formula: Formula::Kane,
+                    }),
+                    se: 1950,
+                    cyl: Some(Cyl {
+                        power: -015,
+                        axis: 90,
+                    }),
+                    bounds: PhantomData,
+                }
+                .check()
+                .unwrap(),
+            ),
+
+            date: NaiveDate::from_ymd_opt(2022, 3, 15).unwrap(),
+            site: Some("RMH".to_string()),
+            sia: None,
+
+            iol: Some(
+                OpIol::<Unchecked> {
+                    iol: Iol {
+                        model: "zxr100".to_string(),
+                        name: "Symfony".to_string(),
+                        company: "Johnson and Johnson".to_string(),
+                        focus: Focus::Edof,
+                        toric: true,
+                    },
+                    se: 2400,
+                    cyl: Some(Cyl {
+                        power: 100,
+                        axis: 90,
+                    }),
+                    bounds: PhantomData,
+                }
+                .check()
+                .unwrap(),
+            ),
+
+            adverse: Some(Adverse::Pc),
+
+            va: OpVa {
+                before: BeforeVa {
+                    best: Va {
+                        num: 600,
+                        den: 1200,
+                    },
+                    raw: None,
+                },
+                after: AfterVa {
+                    best: None,
+                    raw: Va { num: 600, den: 500 },
+                },
+            },
+
+            refraction: OpRefraction {
+                before: Refraction::<Unchecked> {
+                    sph: 300,
+                    cyl: Some(Cyl {
+                        power: -125,
+                        axis: 45,
+                    }),
+                    bounds: PhantomData,
+                }
+                .check()
+                .unwrap(),
+                after: Refraction::<Unchecked> {
+                    sph: -025,
+                    cyl: Some(Cyl {
+                        power: -025,
+                        axis: 60,
+                    }),
+                    bounds: PhantomData,
+                }
+                .check()
+                .unwrap(),
+            },
+        }
+    }
 
     #[tokio::test]
     async fn inserts_sia() {
@@ -19,8 +138,10 @@ mod tests {
 
         // todo:
         // - [x] test named args on the simple example of Sia
-        // - [ ] test getting the client from context
         // - [ ] write a larger query to insert a full Cas
+        //
+        // Within components, the client will be provided via:
+        // let client = expect_context::<Client>();
         let client = create_client().await.expect("DB client to be created");
         let query = "insert Sia { power := <int32>$sia_power, axis := <int32>$sia_axis };";
 
