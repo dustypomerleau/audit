a050047e0a301d104f4074718a00ee5a449af134
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-02-02 11:32:54 +1100

    fix(auth): `impl IntoResponse for AuthError`

--- src/routes/trash.rs
+++ src/routes/trash.rs
@@ -1,9 +1,13 @@
 use super::BASE_AUTH_URL;
-use axum::{extract::Query, response::Redirect};
+use axum::{
+    extract::Query,
+    response::{IntoResponse, Redirect, Response},
+};
 use axum_extra::{
     extract::{cookie::Cookie, CookieJar},
     TypedHeader,
 };
+use axum_macros::debug_handler;
 use serde::Deserialize;
 use std::{collections::HashMap, error::Error};
 use thiserror::Error;
@@ -20,10 +24,19 @@ pub enum AuthError {
     Err,
 }
 
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        match self {
+            Self::Err => "Authentication error".into_response(),
+        }
+    }
+}
+
 async fn handle_sign_in() -> Result<(), AuthError> {
     Ok(())
 }
 
+#[debug_handler]
 pub async fn trash(
     Query(Params { code }): Query<Params>,
     jar: CookieJar,
