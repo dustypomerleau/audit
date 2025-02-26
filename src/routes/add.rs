4122eaf6b76e961a9d443493391488807e72ee4f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-02-26 11:42:29 +1100

    chore(auth): add a temporary link to test session kills

--- src/routes/add.rs
+++ src/routes/add.rs
@@ -1,6 +1,11 @@
-use leptos::{component, view, IntoView};
+use leptos::{IntoView, component, html::ElementChild, view};
 
 #[component]
 pub fn Add() -> impl IntoView {
-    view! { "add a new case" }
+    view! {
+        "add a new case"
+        <a href="/killsession" rel="external">
+            "log out"
+        </a>
+    }
 }
