23c3f45bf1948391d6369b33f15934cf536da0d8
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-03-02 13:47:45 +1100

    refactor(routes): move signin link to the landing page

--- src/routes/landing.rs
+++ src/routes/landing.rs
@@ -2,5 +2,13 @@ use leptos::prelude::{ElementChild, IntoView, component, view};
 
 #[component]
 pub fn Landing() -> impl IntoView {
-    view! { "the landing page!" }
+    view! {
+        "the landing page!"
+        <div style={"flex"}>
+            <a href={"/signin"} rel={"external"} style={"margin-right: 20px"}>
+                sign in (requires Google)
+            </a>
+            <a href={"https://accounts.google.com"}>or create a new google account</a>
+        </div>
+    }
 }
