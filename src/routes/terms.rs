3f3fd0009641bed2efa157b275f317528ed17ca0
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-03-03 10:08:53 +1100

    chore(notes): clarify actions after agreeing to terms

--- src/routes/terms.rs
+++ src/routes/terms.rs
@@ -2,7 +2,8 @@ use leptos::prelude::{ElementChild, IntoView, component, view};
 
 #[component]
 pub fn Terms() -> impl IntoView {
-    // a button, which upon clicking `I agree` calls a server route that sets the current
-    // surgeon's `terms` property to `datetime_current()` and redirects to `add`
+    // Clicking on "I agree":
+    // 1. sets the current surgeon's `terms` property to `datetime_current()`
+    // 2. redirects to `/add`
     view! { "agree to the terms before proceeding" }
 }
