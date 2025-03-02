eaaa203ecfd2d153287048edd4c26607dc93c89f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-03-03 10:13:20 +1100

    chore(notes)

--- src/routes/protected.rs
+++ src/routes/protected.rs
@@ -5,7 +5,7 @@ use leptos_router::components::Outlet;
 #[component]
 pub fn Protected() -> impl IntoView {
     // todo: the signal checks not only that `app_state.surgeon.is_some()`, but also that
-    // `app_state.surgeon.terms` is a Some value before the present time
+    // `app_state.surgeon.terms.is_some()`
     view! {
         <Nav />
         <Outlet />
