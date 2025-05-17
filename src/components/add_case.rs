5ec96a67ec240c04d0c9a65e71d79c763f140db3
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-05-17 18:50:59 +1000

    wip: case insert

--- src/components/add_case.rs
+++ src/components/add_case.rs
@@ -1,7 +1,20 @@
-use crate::case::FormCase;
-use leptos::prelude::{
-    ActionForm, ElementChild, IntoView, ServerAction, ServerFnError, StyleAttribute, component,
-    server, view,
+use crate::{
+    biometry::{Biometry, K, Ks},
+    bounded::Bounded,
+    case::{Case, FormCase, Side, SurgeonCase},
+    iol::{Iol, OpIol},
+    refraction::{OpRefraction, Refraction},
+    sia::Sia,
+    surgeon::{Site, Surgeon, get_current_surgeon},
+    target::Target,
+    va::{AfterVa, BeforeVa, OpVa, Va},
+};
+use leptos::{
+    prelude::{
+        ActionForm, ElementChild, IntoView, ServerAction, ServerFnError, StyleAttribute, component,
+        server, view,
+    },
+    server_fn::error::NoCustomError,
 };
 
 #[component]
@@ -22,7 +35,8 @@ pub fn AddCase() -> impl IntoView {
                 "Enter the case details (fields are required unless marked optional)"
                 // todo: "To change your default values, please [update your profile](link)"
                 <label>
-                    "Unique (anonymised) identifier" <input type="text" name="case[urn]" required />
+                    "Unique (anonymised) identifier"
+                    <input type="text" name="case[urn]" required autofocus />
                 </label> <fieldset>
                     <legend>"Side"</legend>
                     <label>
@@ -70,13 +84,34 @@ pub fn AddCase() -> impl IntoView {
                     <input type="number" min=8 max=14 step=0.01 name="case[wtw]" />
                 </label>
                 <label>
-                    // todo: populate the available formulas from the DB
                     // todo: prefill the surgeon's default formula
-                    "Formula" <input list="formulas" value="Kane" name="case[formula]" required />
-                    <datalist id="formulas">
-                        <option value="barrett">"Barrett"</option>
-                        <option value="kane">"Kane"</option>
-                    </datalist>
+                    "Formula" <select name="case[formula]">
+                        <optgroup label="Thick lens formulas">
+                            <option value="barrett">"Barrett"</option>
+                            <option value="evo">"EVO"</option>
+                            <option value="hillrbf">"Hill RBF"</option>
+                            <option value="holladay2">"Holladay 2"</option>
+                            <option value="kane" selected>
+                                "Kane"
+                            </option>
+                            <option value="okulix">"Okulix raytracing"</option>
+                            <option value="olsen">"Olsen"</option>
+                        </optgroup>
+                        <optgroup label="Thin lens formulas">
+                            <option value="haigis">"Haigis"</option>
+                            <option value="hofferq">"Hoffer Q"</option>
+                            <option value="holladay1">"Holladay 1"</option>
+                            <option value="srkt">"SRK/T"</option>
+                        </optgroup>
+                        <optgroup label="Post-refractive formulas">
+                            <option value="ascrskrs">"ASCRS"</option>
+                            <option value="barretttruek">"Barrett True K"</option>
+                            <option value="haigisl">"Haigis-L"</option>
+                        </optgroup>
+                        <optgroup label="Other">
+                            <option value="other">"Not listed"</option>
+                        </optgroup>
+                    </select>
                 </label>
                 <label>
                     "Check here if you use a custom/optimized IOL constant with this formula"
@@ -128,11 +163,22 @@ pub fn AddCase() -> impl IntoView {
                     <input type="number" min=0 max=179 step=1 name="case[iol_axis]" />
                 </label>> <fieldset>
                     <legend>"Adverse event"</legend>
-                    <input type="radio" value="none" name="case[adverse]" required checked />
-                    <input type="radio" value="rhexis" name="case[adverse]" required />
-                    <input type="radio" value="pc" name="case[adverse]" required />
-                    <input type="radio" value="zonule" name="case[adverse]" required />
-                    <input type="radio" value="other" name="case[adverse]" required />
+                    <label>
+                        "None"
+                        <input type="radio" value="none" name="case[adverse]" required checked />
+                    </label>
+                    <label>
+                        "Rhexis"<input type="radio" value="rhexis" name="case[adverse]" required />
+                    </label>
+                    <label>
+                        "PC"<input type="radio" value="pc" name="case[adverse]" required />
+                    </label>
+                    <label>
+                        "Zonule"<input type="radio" value="zonule" name="case[adverse]" required />
+                    </label>
+                    <label>
+                        "Other"<input type="radio" value="other" name="case[adverse]" required />
+                    </label>
                 </fieldset>
                 <div>
                     "Visual acuity"
@@ -314,8 +360,158 @@ pub fn AddCase() -> impl IntoView {
 #[server]
 pub async fn insert_case(case: FormCase) -> Result<(), ServerFnError> {
     // bookmark:
-    // first call into_surgeon_case(case)
-    // then pattern match all of the values in the SurgeonCase and assign to vars
-    // then insert the query unless conflict on urn/side for that surgeon specifically
+    // - [x] call into_surgeon_case(case)
+    // - [x] pattern match all of the values in the SurgeonCase and assign to vars
+    // - [ ] get inner values, handle options with some_or_empty! you may need to rewrite this for
+    // newtypes
+    // - [ ] write the query to insert unless conflict on urn/side for that surgeon specifically
+
+    let SurgeonCase {
+        urn,
+        date,
+        site,
+        case:
+            Case {
+                side,
+                biometry:
+                    Biometry {
+                        al,
+                        ks,
+                        acd,
+                        lt,
+                        cct,
+                        wtw,
+                    },
+                target:
+                    Target {
+                        formula,
+                        custom_constant,
+                        se: target_se,
+                        cyl: target_cyl,
+                    },
+                main,
+                sia:
+                    Sia {
+                        power: sia_power,
+                        axis: sia_axis,
+                    },
+                iol:
+                    OpIol {
+                        iol:
+                            Iol {
+                                model,
+                                name,
+                                company,
+                                focus,
+                                toric,
+                            },
+                        se: iol_se,
+                        axis: iol_axis,
+                    },
+                adverse,
+                va:
+                    OpVa {
+                        before:
+                            BeforeVa {
+                                best:
+                                    Va {
+                                        num: va_best_before_num,
+                                        den: va_best_before_den,
+                                    },
+                                raw: va_raw_before,
+                            },
+                        after:
+                            AfterVa {
+                                best: va_best_after,
+                                raw:
+                                    Va {
+                                        num: va_raw_after_num,
+                                        den: va_raw_after_den,
+                                    },
+                            },
+                    },
+                refraction:
+                    OpRefraction {
+                        before:
+                            Refraction {
+                                sph: ref_before_sph,
+                                cyl: ref_before_cyl,
+                            },
+                        after:
+                            Refraction {
+                                sph: ref_after_sph,
+                                cyl: ref_after_cyl,
+                            },
+                    },
+            },
+    } = case.into_surgeon_case().await?;
+
+    let date = date.to_string();
+    let site_name = site.map(|Site { name }| name).unwrap_or("{}".to_string());
+
+    let side = match side {
+        Side::Right => "Side.Right",
+        Side::Left => "Side.Left",
+    };
+
+    // note: We don't need to cast the integer types, because they are just going into a format
+    // string. The <int32> will be inferred based on the object field types in Gel.
+    let (al, flat_k_power, flat_k_axis, steep_k_power, steep_k_axis, acd, lt, cct, wtw) = (
+        al.inner(),
+        ks.flat_power(),
+        ks.flat_axis(),
+        ks.steep_power(),
+        ks.steep_axis(),
+        acd.inner(),
+        lt.inner(),
+        cct.map(|cct| cct.inner().to_string())
+            .unwrap_or("{}".to_string()),
+        wtw.map(|wtw| wtw.inner().to_string())
+            .unwrap_or("{}".to_string()),
+    );
+
+    let query = format!(
+        "r#
+with 
+QueryBiometry := (insert Biometry {{
+    al := {al},
+    ks := (select(insert Ks {{
+        flat := (select(insert K {{ power := {flat_k_power}, axis := {flat_k_axis} }})),
+        steep := (select(insert K {{ power := {steep_k_power}, axis := {steep_k_axis} }})),
+    }})),
+    acd := {acd},
+    lt := {lt},
+    cct := {cct},
+    wtw := {wtw}
+}}), 
+QueryCas := (insert Cas {{
+    side := {side},
+    biometry := (select QueryBiometry),
+    target :=
+    year :=
+    main :=
+    sia :=
+    iol :=
+    adverse :=
+    va :=
+    refraction :=
+}}),
+QuerySurgeonCas := (insert SurgeonCas {{
+    surgeon := (select global cur_surgeon), 
+    urn := {urn},
+    date := {date},
+    site := (select(insert Site {{ 
+        name := {site_name} 
+    }} unless conflict on .name else (select Site))),
+    cas := (select QueryCas)
+}})
+select...choose whatever fields you need to display the item in the list
+        #"
+    );
+    // note: not sure how we enforce that the insert can't have a conflict on (.surgeon, .urn,
+    // .case.side) because of the extra hop
+    // I think what you might need to do is to duplicate the side in both the SurgeonCas and the
+    // Cas, and then have an exclusive constraint on (.surgeon, .urn, .side) in the SurgeonCas and
+    // use a generic `unless conflict` on the SurgeonCas insert, without any conditions.
     Ok(())
 }
--- src/components/nav.rs
+++ src/components/nav.rs
@@ -1,6 +1,15 @@
-use leptos::prelude::{IntoView, component, view};
+use leptos::prelude::{ElementChild, IntoView, StyleAttribute, component, view};
 
 #[component]
 pub fn Nav() -> impl IntoView {
-    view! { "a nav toolbar containing: logo and menu/links to (add, instructions, list, reports)" }
+    view! {
+        <div style="display: grid; grid-template-columns: 1fr 100px; gap: 1rem; padding-bottom: 2rem;">
+            <div>
+                "a nav toolbar containing: logo and menu/links to (add, instructions, list, reports)"
+            </div>
+            <a href="/killsession" rel="external">
+                "log out"
+            </a>
+        </div>
+    }
 }
