07b964d5a40e0964746584c43f51d689232e425e
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-06-21 12:20:08 +1000

    chore(notes): transactional email

--- src/email.rs
+++ src/email.rs
@@ -3,3 +3,8 @@
 // or alternatively:
 // https://github.com/jdrouet/catapulte
 // which is self-hosted
+
+// You can find examples of using the AWS Rust SDK for SES (probably your best option), here:
+// https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/rustv1/examples/ses#code-examples
+// An example of doing this with catapulte is found here:
+// https://github.com/jdrouet/catapulte/blob/main/wiki/with-aws-ses.md
