b2ecea85291c494a89fbb2ee7ec51d0b029f9bbe
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-10-14 14:36:28 +1100

    chore(fmt): prettier

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 A WIP prototype of a comparative surgical audit tool for cataract surgeons.
 The primary focus is refractive outcomes, with particular attention to providing feedback on astigmatic outcomes relative to target.
-
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,8 @@
 const config = {
-    overrides: [{ files: "*.md", options: { tabWidth: 2 } }],
+    overrides: [
+        { files: "*.md", options: { tabWidth: 2 } },
+        { files: "*.svg", options: { parser: "html" } },
+    ],
     printWidth: 100,
     tabWidth: 4,
 };
