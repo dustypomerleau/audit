bc27e8196957daf192593a8b40151cbf2e3cf936
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-10-26 20:21:08 +1100

    fix(md): revert path changes and return to copying markdown to root

--- src/routes/landing.rs
+++ src/routes/landing.rs
@@ -1,4 +1,5 @@
 use std::env::current_exe;
+use std::path::PathBuf;
 
 use leptos::prelude::IntoView;
 use leptos::prelude::component;
@@ -9,13 +10,17 @@ use crate::components::Markdown;
 
 #[component]
 pub fn Landing() -> impl IntoView {
-    let markdown_path_buf = current_exe()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .join("site/markdown/landing.md");
+    // // todo: this fails in prod because the binary is at the root, whereas in dev, the binary is
+    // // inside debug/, so there is an extra call to parent() here that fails in prod.
+    // let markdown_path_buf = current_exe()
+    //     .unwrap()
+    //     .parent()
+    //     .unwrap()
+    //     .parent()
+    //     .unwrap()
+    //     .join("site/markdown/landing.md");
+
+    let markdown_path_buf = PathBuf::from("markdown/landing.md");
 
     view! {
         <Hero />
